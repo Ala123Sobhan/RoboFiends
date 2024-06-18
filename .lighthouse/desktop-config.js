1f7f6fc0167e256027d05fe0dc548104807703bc
Ala123Sobhan <ala123sobhan@gmail.com>
2024-06-18 12:26:44 -0500

    gh-actions cm fix 100

--- .lighthouse/desktop-config.js
+++ .lighthouse/desktop-config.js
@@ -2,7 +2,13 @@ module.exports = {
   ci: {
     collect: {
       settings: {
-        emulatedFormFactor: "desktop",
+        screenEmulation: {
+          mobile: false,
+          width: 1200,
+          height: 800,
+          deviceScaleFactor: 1,
+          disabled: false,
+        },
         onlyCategories: [
           "performance",
           "accessibility",
