f039a81b0bd4273428a22e9e3da6f0b60cd718d8
Ala123Sobhan <ala123sobhan@gmail.com>
2024-06-18 09:49:24 -0500

    gh-actions cm 20

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -4,6 +4,17 @@ const exec = require("@actions/exec");
 
 function run() {
   core.notice("hello from custom action!");
+  // 1) Get some input values
+  const bucket = core.getInput("bucket", { required: true });
+  const bucketRegion = core.getInput("bucket-region", { required: true });
+  const buildFolder = core.getInput("build-folder", { required: true });
+
+  // 2) Upload files
+  const s3Uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${buildFolder} ${s3Uri} --region ${bucketRegion}`);
+
+  const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+  core.setOutput("website-url", websiteUrl); // ::set-output
 }
 
 run();
